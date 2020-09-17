68fdb49af556b79cf0a475e598c6d3b1d225e7f8
Justin Eveland <jeveland@newrelic.com>
2020-09-17 17:32:21 -0400

    feat: add status state module

---
+++ nerdlets/nr1-catalog-manager-nerdlet/constants/Status.js
@@ -0,0 +1,8 @@
+/**
+ * Simple Status type definition for transitioning states within the app
+ */
+module.exports = {
+  LOADING: 'LOADING',
+  USER_TOKEN_SET: 'USER_TOKEN_SET',
+  USER_TOKEN_EMPTY: 'USER_TOKEN_EMPTY'
+};
